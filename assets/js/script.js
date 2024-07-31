2f1f8a72c15508f6eb3db8c9701966a81011a343
TinaGrigorova <tina.grigorova@students.codeinstitute.net>
2024-07-31 12:57:40 +0000

    add questions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+const questions = [
+    {
+        question: "Where is the biggest lake?", 
+        answers: [
+            {text: "Bulgaria", correct: false},
+            {text: "Canada", correct: false},
+            {text: "Asia", correct: true},
+            {text: "Australia", correct: false},
+        ]
+    }, 
+    {
+        question: "Which is the smallest country?", 
+        answers:[
+            {text: "Vatican", correct: true},
+            {text: "Luxembourg", correct: false},
+            {text: "Monaco", correct: false},
+            {text: "Liechtenstein", correct: false},
+        ]
+
+    },
+    {
+        question: "Where is the highest island mountain?", 
+        answers:[
+            {text: "Taiwan - Yu Shan", correct: false},
+            {text: "Tenerife - Teide", correct: false},
+            {text: "Sri Lanka - Pidurutalagala", correct: false},
+            {text: "New Guinea - Puncak Jaya", correct: true},
+        ]
+
+    }
+    
+];
+const questionElement = document.getElementById("questions");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex=0;
+let score= 0;
+
+
 function startQuiz(){
 
 }
@@ -8,5 +48,6 @@ function resetState(){
 
 }
 function selectAnswer(){
-    
+
 }
+console.log("still connected!");
